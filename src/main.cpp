ea9aba29f53c74b4ba99bb0f33269ee02acf468a
Kristoffer Grönlund <krig@koru.se>
2014-11-16 19:55:54 +0100

    open file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include "common.hpp"
+#include "os.hpp"
 
 int main(int argc, char* argv[]) {
-	LOG_INFO("lp");
+	if (argc < 2) {
+		LOG_ERROR("Expected <file>");
+		return 1;
+	}
+
+	const char* infile = argv[argc-1];
+	if (!os::path::exists(infile)) {
+		LOG_ERROR("File not found: %s", infile);
+		return 1;
+	}
+
+	
 	return 0;
 }
