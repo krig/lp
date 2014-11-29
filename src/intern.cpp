f0c792df1fa495b98e794441f6c78bbad9fc9926
Kristoffer Grönlund <krig@koru.se>
2014-11-29 23:40:11 +0100

    Well, when I removed the destructor I might as well revert to storing a pointer.

--- src/intern.cpp
+++ src/intern.cpp
@@ -2,34 +2,28 @@
 #include "intern.hpp"
 
 namespace {
-	struct interned_string {
-		interned_string(const char* str, bool copy) : str(copy ? strdup(str) : str), copy(copy) {}
-		const char* str;
-		bool copy;
-	};
-
 	struct hash_str {
-		size_t operator()(const interned_string& str) const {
+		size_t operator()(const char* str) const {
 			size_t h = 0;
-			for (size_t i = strlen(str.str); i != 0; --i)
-				h = 31 * h + str.str[i-1];
+			for (size_t i = strlen(str); i != 0; --i)
+				h = 31 * h + str[i-1];
 			return h;
 		}
 	};
 
 	struct str_equal {
-		bool operator()(const interned_string& a, const interned_string& b) const {
-			return strcmp(a.str, b.str) == 0;
+		bool operator()(const char* a, const char* b) const {
+			return strcmp(a, b) == 0;
 		}
 	};
 
-	std::unordered_set<interned_string, hash_str, str_equal> _interned;
+	std::unordered_set<const char*, hash_str, str_equal> _interned;
 }
 
 const char* intern_string(const char* str, bool copy)
 {
-	auto i = _interned.find(interned_string(str, false));
+	auto i = _interned.find(str);
 	if (i == _interned.end())
-		i = _interned.emplace(str, copy).first;
-	return i->str;
+		i = _interned.emplace(copy ? strdup(str) : str).first;
+	return *i;
 }
