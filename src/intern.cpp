48eb5087337b1c732710dfe2fa4ab1dd5ffe8776
Kristoffer Grönlund <krig@koru.se>
2014-11-29 23:15:20 +0100

    interning: doing it C++ style (actually, this is sort of nice)

--- src/intern.cpp
+++ src/intern.cpp
@@ -2,41 +2,41 @@
 #include "intern.hpp"
 
 namespace {
+	struct interned_string {
+		interned_string(const char* str, bool copy) : str(str), copy(copy) {
+			if (copy)
+				this->str = strdup(str);
+		}
+		~interned_string() {
+			if (copy)
+				free((void*)str);
+		}
+		const char* str;
+		bool copy;
+	};
 
 	struct hash_str {
-		size_t operator()(const char* str) const {
+		size_t operator()(const interned_string& str) const {
 			size_t h = 0;
-			for (size_t i = strlen(str); i != 0; --i)
-				h = 31 * h + str[i];
+			for (size_t i = strlen(str.str); i != 0; --i)
+				h = 31 * h + str.str[i];
 			return h;
 		}
 	};
 
 	struct str_equal {
-		bool operator()(const char* a, const char* b) const {
-			return strcmp(a, b) == 0;
+		bool operator()(const interned_string& a, const interned_string& b) const {
+			return strcmp(a.str, b.str) == 0;
 		}
 	};
 
-	std::unordered_set<const char*, hash_str, str_equal> _interned;
-
-	const char* copy_string(const char* str)
-	{
-		// TODO: block allocate from arena allocator?
-		// as long as I never free these strings, it
-		// doesn't really matter much.
-		return strdup(str);
-	}
+	std::unordered_set<interned_string, hash_str, str_equal> _interned;
 }
 
 const char* intern_string(const char* str, bool copy)
 {
-	auto i = _interned.find(str);
-	if (i == _interned.end()) {
-		if (copy)
-			str = copy_string(str);
-		_interned.insert(str);
-		return str;
-	}
-	return *i;
+	auto i = _interned.find(interned_string(str, false));
+	if (i == _interned.end())
+		i = _interned.emplace(str, copy).first;
+	return i->str;
 }
