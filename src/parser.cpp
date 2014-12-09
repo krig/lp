a15b4bd690d42e767926d6c6e9c37a2954af0fc3
Kristoffer Grönlund <krig@koru.se>
2014-12-09 17:54:42 +0100

    simplify parser layout

--- src/parser.cpp
+++ src/parser.cpp
@@ -3,157 +3,66 @@
 #include "parser.hpp"
 #include "module.hpp"
 
-void parser_state::init(lexer_state *mainfile)
+struct ast_expr;
+struct parse_error;
+
+struct parser_result {
+	enum result_type {
+		Error,
+		Mismatch,
+		Expr
+	} type;
+	union {
+		ast_expr* _expr;
+		parse_error* _error;
+	};
+
+	bool mismatch() const { return type == Mismatch; }
+	ast_expr* expr() const { return (type == Expr) ? _expr : nullptr; }
+	parse_error* error() const { return (type == Error) ? _error : nullptr; }
+
+	parser_result() : type(Mismatch), _expr(nullptr) {}
+	explicit parser_result(ast_expr* expr) : type(Expr), _expr(expr) {}
+	explicit parser_result(parse_error* error) : type(Error), _error(error) {}
+};
+
+struct parse_error {
+	virtual ~parse_error() {}
+	virtual const char* what() = 0;
+};
+
+typedef parser_result ParseFunction(parser_state* state);
+
+parser_result parse_toplevel(parser_state* state)
 {
-	_mainfile = mainfile;
+	return parser_result();
 }
 
-unique_ptr<ast::ident> parser_state::parse_ident()
+
+void parser_state::init(lexer_state* lexer)
 {
-	if (!match(T_IDENTIFIER)) {
-		error("Expected identifier, got %s", get_token(0).to_str().c_str());
-		throw ::error("Expected identifier, got %s", get_token(0).to_str().c_str());
-	}
-	unique_ptr<ast::ident> id(new ast::ident);
-	id->text = get_token(0).text;
-	advance();
-	return id;
+	this->lexer = lexer;
 }
 
-unique_ptr<ast::arglist> parser_state::parse_arglist()
-{
-	if (!match(T_LPAREN))
-		return unique_ptr<ast::arglist>();
-	advance();
-
-	//vector<argument*> args;
-
-	if (!match(T_RPAREN))
-		return unique_ptr<ast::arglist>();
-	return unique_ptr<ast::arglist>();
-}
-
-unique_ptr<ast::retlist> parser_state::parse_retlist()
-{
-	match(T_LPAREN);
-	match(T_RPAREN);
-	return unique_ptr<ast::retlist>();
-}
-
-unique_ptr<ast::block> parser_state::parse_block()
-{
-	match(T_LBRACE);
-	match(T_RBRACE);
-	return unique_ptr<ast::block>();
-}
-
-unique_ptr<ast::fundecl> parser_state::parse_fundecl()
-{
-	auto al = parse_arglist();
-	if (match(T_ARROW)) {
-		auto rl = parse_retlist();
-	}
-	auto bl = parse_block();
-	return unique_ptr<ast::fundecl>();
-}
-
-unique_ptr<ast::fundef> parser_state::parse_fundef()
-{
-	unique_ptr<ast::fundef> fundef(new ast::fundef);
-	advance();
-	auto id = parse_ident();
-	auto fd = parse_fundecl();
-	fundef->_name = id.release();
-	fundef->_fundecl = fd.release();
-	printf("fundef: %s\n", fundef->_name->text());
-	return fundef;
-}
-
-bool parser_state::advance()
-{
-	if (_tokens.size() > 0)
-		_tokens.pop_front();
-	if (_tokens.size() == 0) {
-		token t = _mainfile->next_token();
-		if (t.type != T_EOF)
-			_tokens.push_back(t);
-	}
-	if (_tokens.size() > 0) {
-		auto desc = get_token(0).to_str();
-		LOG_INFO("%s", desc.c_str());
-	}
-	return _tokens.size() > 0;
-}
-
-token parser_state::get_token(int n) {
-	while (n > _tokens.size())
-		_tokens.push_back(_mainfile->next_token());
-	return _tokens.at(n);
-}
-
-
-bool parser_state::match(Token type)
-{
-	if (get_token(0).type == type) {
-		return true;
-	}
-	return false;
-}
-
-bool parser_state::match_keyword(const char* kw)
-{
-	token t = get_token(0);
-	if (t.type == T_KEYWORD && t.text == kw) {
-		return true;
-	}
-	return false;
-}
 
 module *parser_state::parse()
 {
 	printf("parsing...\n");
 	module* m = new module("main");
-	m->_files.push_back(_mainfile->_file);
+	m->_files.push_back(lexer->_file);
 
-	// AST building:
-	// while there are more tokens
-	// match a toplevel statement;
-	//
-	// from AST build in-memory program representation:
-	// while incomplete;
-	// generate information about functions and types
-	// evaulate compile-time expressions
-
-	/*
-	token t = get_token(0);
-	switch (t.type) {
-	case T_EOF:
-		return m;
-	case T_KEYWORD: {
-		if (t.text == "def") {
-			auto v = parse_fundef();
-			if (v)
-				m->stmts.push_back(v.release());
-		} else if (t.text == "use") {
+	while (true) {
+		auto result = parse_toplevel(this);
+		if (result.error()) {
+			error("Parse error: %s", result.error()->what());
+		} else if (result.expr()) {
+			// TODO: handle successful parse
+		} else {
+			// TODO
+			break;
 		}
-	} break;
-	case T_IDENTIFIER: {
-		advance();
-		token op = get_token(0);
-		if (op.type == T_DOUBLE_COLON) {
-			// immutable definition
-		} else if (op.type == T_COLON_ASSIGN) {
-			// mutable definition
-		} else if (op.type == T_COLON) {
-			// typed mutable definition
-		}
-	} break;
-	default: {
-		LOG_ERROR("Unexpected token: %s", t.to_str().c_str());
-		return nullptr;
-	};
 	}
-	*/
+
 	return m;
 }
 
@@ -167,13 +76,8 @@ void parser_state::error(const char* fmt, ...)
 
 	const char* f;
 	int l;
-	if (_tokens.size() > 0) {
-		f = _tokens.front()._file;
-		l = _tokens.front()._line;
-	} else {
-		f = _mainfile->_file;
-		l = _mainfile->_stream._line;
-	}
+	f = lexer->_file;
+	l = lexer->_stream._line;
 
 	logging::log_context(f, l, "").error("Parser error: %s", buf);
 }
@@ -188,13 +92,8 @@ void parser_state::warning(const char* fmt, ...)
 
 	const char* f;
 	int l;
-	if (_tokens.size() > 0) {
-		f = _tokens.front()._file;
-		l = _tokens.front()._line;
-	} else {
-		f = _mainfile->_file;
-		l = _mainfile->_stream._line;
-	}
+	f = lexer->_file;
+	l = lexer->_stream._line;
 
 	logging::log_context(f, l, "").warn("Parser warning: %s", buf);
 }
--- src/parser.hpp
+++ src/parser.hpp
@@ -5,97 +5,12 @@
 struct lexer_state;
 struct module;
 
-namespace ast {
-	struct node;
-	struct def;
-	struct fundecl;
-	struct block;
-	struct capture;
-	struct stmt;
-	struct ident;
-	struct expr;
-
-	struct type_information {
-	};
-
-	struct node {
-	};
-
-	struct stmt : node {
-	};
-
-	struct expr : node {
-		ident* _type;
-	};
-
-	struct leaf : node {
-	};
-
-	struct ident : leaf {
-		symbol text;
-	};
-
-	struct vardef : stmt {
-		ident* _name;
-		ident* _type;
-		expr* _expr;
-		bool _mutable;
-	};
-
-	struct arglist : leaf {
-		//vector<> args;
-	};
-
-	struct retlist : leaf {
-		//vector<> rets;
-	};
-
-	struct fundecl : expr {
-		arglist* _arglist;
-		retlist* _retlist;
-		block* _block;
-	};
-
-	struct fundef : stmt {
-		ident* _name;
-		fundecl* _fundecl;
-	};
-
-	struct block : expr {
-		capture* _capture;
-		vector<stmt*> _body;
-	};
-
- 	struct unit : node {
-		vector<stmt*> stmts;
-	};
-
-}
-
 struct parser_state {
-
-	void init(lexer_state *mainfile);
-
+	void init(lexer_state* lexer);
 	module *parse();
-
-	unique_ptr<ast::ident> parse_ident();
-	unique_ptr<ast::arglist> parse_arglist();
-	unique_ptr<ast::retlist> parse_retlist();
-	unique_ptr<ast::block> parse_block();
-	unique_ptr<ast::fundecl> parse_fundecl();
-	unique_ptr<ast::fundef> parse_fundef();
-
-	bool advance();
-	token get_token(int n);
-
-	bool match(Token type);
-	bool match_keyword(const char* kw);
 
 	void error(const char* fmt, ...);
 	void warning(const char* fmt, ...);
 
-	lexer_state *_mainfile;
-	module *_module;
-
-	std::deque<token> _tokens;
+	lexer_state* lexer;
 };
